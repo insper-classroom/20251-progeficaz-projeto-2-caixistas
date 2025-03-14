b2a4ef1434535a2353d613e282585a4fee536a32
felipecameloss <felipecss@al.insper.edu.br>
2025-03-14 16:25:02 -0300

    Implementação do teste de atualizar imovel

--- test_api.py
+++ test_api.py
@@ -159,3 +159,35 @@ def test_get_imoveis_tipo(mock_connect_db, imovel):
     } 
 
     assert response.get_json() == expected_response
+
+@patch("servidor.connect_db")
+def test_atualiza_imovel(mock_connect_db, imovel):
+
+    # Criação do Mock para a conexão e cursor
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+
+    # O mock retorna o cursor quando chamamos o conn.cursor()
+    mock_conn.cursor.return_value = mock_cursor
+
+    # Simulação do banco de dados
+    mock_cursor.fetchall.return_value = [
+        (1, "Vereador", "Rua", "Centro", "Bofete", "18590-000", "casa", 50000, "2025-03-11"),
+        (2, "Miguel Damha", "Avenida", "Damha", "São José do Rio Preto", "15061-800", "casa em condominio", 50000, "2025-03-11"),
+    ]
+
+    #Chama a conexão do Mock ao invés da conexão real
+    mock_connect_db.return_value = mock_conn
+
+    # Faz a requisição GET para a API na rota /
+    response = imovel.get("/imoveis/atualiza/1/tipo/apartamento")
+
+    # Vendo se o status da resposta é 200
+    assert response.status_code == 200
+
+    # Respostas esperadas
+    expected_response = {
+        'mensagem': 'Imóvel atualizado com sucesso.'
+    }
+
+    assert response.get_json() == expected_response
